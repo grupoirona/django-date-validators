02a708a86180b808d7a6b68960327ba67b458b03
Mariano Baragiola <mariano@baragiola.com.ar>
2016-08-31 10:37:41 -0300

    ADD initial files

---
+++ django_date_validators/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjangoDateValidatorsConfig(AppConfig):
+    name = 'django_date_validators'
---
+++ django_date_validators/validators.py
@@ -0,0 +1,72 @@
+import datetime
+
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+
+
+def date_is_future(value):
+    if isinstance(value, datetime.date):
+        if value <= datetime.date.today():
+            raise ValidationError(
+                _("The date entered must be greater than today.")
+            )
+    elif isinstance(value, datetime.datetime):
+        if value.date() <= datetime.today():
+            raise ValidationError(
+                _("The date entered must be greater than today.")
+            )
+    else:
+        raise ValidationError(
+            _("The value entered isn't a valid type of date or datetime.")
+        )
+
+
+def date_is_present_or_future(value):
+    if isinstance(value, datetime.date):
+        if value < datetime.date.today():
+            raise ValidationError(
+                _("The date entered must be today or lesser.")
+            )
+    elif isinstance(value, datetime.datetime):
+        if value.date() < datetime.datetime.today():
+            raise ValidationError(
+                _("The date entered must be today or greater.")
+            )
+    else:
+        raise ValidationError(
+            _("The value entered isn't a valid type of date or datetime.")
+        )
+
+
+def date_is_past(value):
+    if isinstance(value, datetime.date):
+        if value >= datetime.date.today():
+            raise ValidationError(
+                _("The date entered must be today or lesser.")
+            )
+    elif isinstance(value, datetime.datetime):
+        if value.date() >= datetime.datetime.today():
+            raise ValidationError(
+                _("The date entered must be lesser than today.")
+            )
+    else:
+        raise ValidationError(
+            _("The value entered isn't a valid type of date or datetime.")
+        )
+
+
+def date_is_present_or_past(value):
+    if isinstance(value, datetime.date):
+        if value > datetime.date.today():
+            raise ValidationError(
+                _("The date entered must be today or lesser.")
+            )
+    elif isinstance(value, datetime.datetime):
+        if value.date() > datetime.datetime.today():
+            raise ValidationError(
+                _("The date entered must be today or lesser.")
+            )
+    else:
+        raise ValidationError(
+            _("The value entered isn't a valid type of date or datetime.")
+)
