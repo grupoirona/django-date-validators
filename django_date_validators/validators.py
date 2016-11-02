fb6744f5a8b440ec17fda3a9f23c82e08285085c
Mariano Baragiola <mariano@baragiola.com.ar>
2016-11-02 18:23:08 -0300

    IMP validators.py Added timezone instead of datetime.

--- django_date_validators/validators.py
+++ django_date_validators/validators.py
@@ -1,17 +1,18 @@
-import datetime
+from datetime import date, datetime
 
 from django.core.exceptions import ValidationError
+from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
 
 def date_is_future(value):
-    if isinstance(value, datetime.date):
-        if value <= datetime.date.today():
+    if isinstance(value, datetime):
+        if value <= timezone.now():
             raise ValidationError(
                 _("The date entered must be greater than today.")
             )
-    elif isinstance(value, datetime.datetime):
-        if value.date() <= datetime.today():
+    elif isinstance(value, date):
+        if value <= date.today():
             raise ValidationError(
                 _("The date entered must be greater than today.")
             )
@@ -22,15 +23,15 @@ def date_is_future(value):
 
 
 def date_is_present_or_future(value):
-    if isinstance(value, datetime.date):
-        if value < datetime.date.today():
-            raise ValidationError(
-                _("The date entered must be today or lesser.")
-            )
-    elif isinstance(value, datetime.datetime):
-        if value.date() < datetime.datetime.today():
+    if isinstance(value, datetime):
+        if value < timezone.now():
             raise ValidationError(
                 _("The date entered must be today or greater.")
+            )
+    elif isinstance(value, date):
+        if value < date.today():
+            raise ValidationError(
+                _("The date entered must be today or lesser.")
             )
     else:
         raise ValidationError(
@@ -39,15 +40,15 @@ def date_is_present_or_future(value):
 
 
 def date_is_past(value):
-    if isinstance(value, datetime.date):
-        if value >= datetime.date.today():
-            raise ValidationError(
-                _("The date entered must be today or lesser.")
-            )
-    elif isinstance(value, datetime.datetime):
-        if value.date() >= datetime.datetime.today():
+    if isinstance(value, datetime):
+        if value >= timezone.now():
             raise ValidationError(
                 _("The date entered must be lesser than today.")
+            )
+    if isinstance(value, date):
+        if value >= date.today():
+            raise ValidationError(
+                _("The date entered must be today or lesser.")
             )
     else:
         raise ValidationError(
@@ -55,14 +56,14 @@ def date_is_past(value):
         )
 
 
-def date_is_past_or_present(value):
-    if isinstance(value, datetime.date):
-        if value > datetime.date.today():
+def date_is_present_or_past(value):
+    if isinstance(value, datetime):
+        if value > timezone.now():
             raise ValidationError(
                 _("The date entered must be today or lesser.")
             )
-    elif isinstance(value, datetime.datetime):
-        if value.date() > datetime.datetime.today():
+    elif isinstance(value, date):
+        if value > date.today():
             raise ValidationError(
                 _("The date entered must be today or lesser.")
             )
